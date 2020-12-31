d0abe146c498ee00d0e5b216d840162d70a7dbf9
Nicholas de Jong <me@nicholasdejong.com>
2020-12-31 14:55:36 +1000

    (pyvboxmanage) bump version to 0.3.1

--- src/pyvboxmanage/__init__.py
+++ src/pyvboxmanage/__init__.py
@@ -1,7 +1,7 @@
 
 __title__ = 'pyvboxmanage'
 __author__ = 'Nicholas de Jong <me@nicholasdejong.com>'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 __exec_timeout_default__ = 120
 __config_import_recursion_limit__ = 20
